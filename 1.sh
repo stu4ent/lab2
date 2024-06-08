337d474da3a17dd3d3b994ecd75f13e933af1660
stu4ent <bahinskyi.o@donnu.edu.ua>
2024-06-09 00:18:42 +0300

    Initial commit

--- 1.sh
+++ 1.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-# Check if the correct number of arguments are provided
-if [ "$#" -ne 2 ]; then
-    echo "Usage: path to directory and remote repository url"
-    exit 1
+if [ $# -ne 2 ]; then
+	echo "Directory route and Repo-URL: "
+	exit 1
 fi
 
-# Assign arguments to variables
-DIRECTORY=$1
-REMOTE_REPO=$2
+directory="$1"
+github_repo="$2"
 
-# Initialize local git repository in the specified directory
-cd $DIRECTORY
-git init
+git init "$directory"
 
-# Add all files in the specified directory
+cd "directory"
+
 git add .
 
-# Commit the changes with the message "Initial commit"
 git commit -m "Initial commit"
 
-# Add the remote repository
-git remote add origin $REMOTE_REPO
-
-# Push the changes to the remote repository
-git push -u origin master
-
-echo "Successfully moved $DIRECTORY content to $REMOTE_REPO"
+git remote add origin "$github_repo"
